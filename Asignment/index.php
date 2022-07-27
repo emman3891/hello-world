e43ba3889093525805326b1f1d29950eeb8ad53a
emman3891 <110010109+emman3891@users.noreply.github.com>
2022-07-27 04:57:43 -0700

    Update index.php

--- Asignment/index.php
+++ Asignment/index.php
@@ -101,7 +101,7 @@
         $sql = "SELECT * FROM drugs";
         if($result = mysqli_query($link, $sql))
         {
-            
+           if(mysqli_num_rows($result) > 0)
             {
              
              while($row = mysqli_fetch_array($result))
